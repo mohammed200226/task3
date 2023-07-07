9f4adc5011c7085558aa83dfa9f71382684feff8
mohammed200226 <133527086+mohammed200226@users.noreply.github.com>
2023-07-07 23:36:45 +0300

    Create task3(1).php

---
+++ task3(1).php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $pageTitle ?></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+</head>
+<style>
+    body {
+        font-family: 'Raleway', sans-serif;
+    }
+</style>
+
+<body>
+    <nav>
+        <a href="task3(6).php">Home</a>|
+        <a href="task3(3).php">about</a>|
+        <a href="task3(4).php">contact</a>
+    </nav>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.min.js"></script>
+
+</html>
