296dd6f389efd146907cef23aa95a0fab2984f4f
mohammed200226 <133527086+mohammed200226@users.noreply.github.com>
2023-07-07 23:39:02 +0300

    Create task3(4).php

---
+++ task3(4).php
@@ -0,0 +1,9 @@
+<?php
+
+$users = [
+    'user1' => ['name' => 'mohammed', 'age' => 21, 'clr' => 'blue'],
+    'user2' => ['name' => 'saher', 'age' => 22, 'clr' => 'red'],
+    'user3' => ['name' => 'tarak', 'age' => 23, 'clr' => 'green'],
+    'user4' => ['name' => 'ali', 'age' => 25, 'clr' => 'yellow'],
+    'user5' => ['name' => 'ahmed', 'age' => 24, 'clr' => 'pink'],
+];
